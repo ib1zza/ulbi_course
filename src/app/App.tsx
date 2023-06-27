c20e761cadceba6690f3091e9c5ae41ada53a0d3
Mikhail Pshenichnyy <dremast1337@gmail.com>
2023-06-27 16:11:08 +0300

    added config for react-router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,22 +5,18 @@ import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider/lib/useTheme";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={classNames('app',{}, [theme] )}>
+        <div className={classNames('app', {}, [theme])}>
             <Link to={"/about"}>About</Link>
             <Link to={"/"}>Main</Link>
             <button onClick={toggleTheme}>Toggle</button>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/about" element={<AboutPage/>}/>
-                    <Route path="/" element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
